1c53edb3a90cbb904e923c95f00f9588895b66d9
Will Squibb <wsquibb@andrew.cmu.edu>
2024-02-23 14:21:41 -0500

    Remove prior verifications

--- server/index.js
+++ server/index.js
@@ -102,22 +102,6 @@ app.post("/sendmessage", (req,res) => {
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// Twilio verification for clickable links in text messages
-app.get('/b38a6c86e62761d3561f58c0be47e378.html', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'b38a6c86e62761d3561f58c0be47e378.html'));
-});
-
-// TLS certificate verification for Let's Encrypt (1)
-app.get('/.well-known/acme-challenge/0urWIe3t1qXli7jmEpAEWd1dcUHFKI51i32L5BNK4dI', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public/.well-known/acme-challenge/0urWIe3t1qXli7jmEpAEWd1dcUHFKI51i32L5BNK4dI'));
-});
-
-// TLS certificate verification for Let's Encrypt (2)
-app.get('/.well-known/acme-challenge/BLghGRk4WelBdAbLQdFuh80jpm2xhk6ShnqKHWBP9Mo', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public/.well-known/acme-challenge/BLghGRk4WelBdAbLQdFuh80jpm2xhk6ShnqKHWBP9Mo'));
-});
-
-
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
