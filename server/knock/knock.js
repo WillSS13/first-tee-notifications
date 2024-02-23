ce03cf18f49b290ed55e4a5c88c92f8f1b5c1c6d
Will Squibb <wsquibb@andrew.cmu.edu>
2024-02-23 16:28:36 -0500

    fix horrible mistake

--- server/knock/knock.js
+++ server/knock/knock.js
@@ -15,6 +15,21 @@ const sendSMS = (id, phone, message) => {
   .catch((error) => console.error(error));
 }
 
+const sendEmail = (id, email, message) => {
+  knock.workflows.trigger("email", {
+    data: { message },
+    recipients: [
+      {
+        id: id,
+        email: email
+        // email: "notifications@thefirstteepittsburgh.org"
+      },
+    ],
+  })
+  .then((response) => console.log(response))
+  .catch((error) => console.error(error));
+}
+
 const testSMS = (id, phone, message) => {
   knock.workflows.trigger("sms", {
     data: { message },
@@ -44,4 +59,4 @@ const testEmail = (id, email, message) => {
   .catch((error) => console.error(error));
 }
 
-module.exports = { testSMS, testEmail };
+module.exports = { testSMS, testEmail, sendSMS, sendEmail };
