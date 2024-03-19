4e896e64ed7bebc650c7bfc1a1afb19ac096641e
Bchenjamin <97626217+Bchenjamin@users.noreply.github.com>
2024-03-18 21:11:47 -0400

    added specs on knock.js, cleanuped salesforce.js (#112)

--- server/knock/knock.js
+++ server/knock/knock.js
@@ -1,6 +1,15 @@
 const { Knock } = require("@knocklabs/node");
 const knock = new Knock(process.env.KNOCK_API_KEY);
 
+// sendSMS
+// params: id, phone, message
+/**
+ * Sends an SMS message to a recipient.
+ *
+ * @param {string} id - Knock ID.
+ * @param {string} phone - The phone number of the recipient.
+ * @param {string} message - The message to be sent.
+ */
 const sendSMS = (id, phone, message) => {
   knock.workflows.trigger("sms", {
     data: { message },
@@ -15,6 +24,15 @@ const sendSMS = (id, phone, message) => {
   .catch((error) => console.error(error));
 }
 
+// sendEmail
+// params: id, email, message
+/**
+ * Sends an email message to a recipient.
+ *
+ * @param {string} id - Knock ID.
+ * @param {string} email - The email of the recipient.
+ * @param {string} message - The message to be sent.
+ */
 const sendEmail = (id, email, message) => {
   knock.workflows.trigger("email", {
     data: { message },
@@ -30,33 +48,4 @@ const sendEmail = (id, email, message) => {
   .catch((error) => console.error(error));
 }
 
-const testSMS = (id, phone, message) => {
-  knock.workflows.trigger("sms", {
-    data: { message },
-    recipients: [
-      {
-        id: id,
-        phone_number: phone,
-      },
-    ],
-  })
-  .then((response) => console.log(response))
-  .catch((error) => console.error(error));
-}
-
-const testEmail = (id, email, message) => {
-  knock.workflows.trigger("email", {
-    data: { message },
-    recipients: [
-      {
-        id: id,
-        email: email
-        // email: "notifications@thefirstteepittsburgh.org"
-      },
-    ],
-  })
-  .then((response) => console.log(response))
-  .catch((error) => console.error(error));
-}
-
-module.exports = { testSMS, testEmail, sendSMS, sendEmail };
+module.exports = { sendSMS, sendEmail };
