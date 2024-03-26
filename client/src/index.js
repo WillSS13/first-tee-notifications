3812688a1c50f6d3a7bfcb7c326ff9791ce21b43
Bchenjamin <bzchen@andrew.cmu.edu>
2024-03-26 12:56:58 -0400

    added authentication

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import ClassList from './pages/classList';
 import Message from './pages/message';
+import Unauthorized from './pages/unauthorized';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
     path: "/message",
     element: <Message />,
   },
+  {
+    path: "/unauthorized",
+    element: <Unauthorized />,
+  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
