735d29d20183a7a7a52d7dbce9db5fb0b8de6092
Bchenjamin <bzchen@andrew.cmu.edu>
2024-03-27 14:23:55 -0400

    made unauthorized page look better, added redirect

--- client/src/pages/unauthorized.js
+++ client/src/pages/unauthorized.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import '../App.css';
+import { useNavigate } from "react-router-dom";
+
 function Unauthorized() {
+    const navigate = useNavigate();
+    const redirectToTarget = () => {
+        navigate('/');
+      };
     return (
         <>
-            <h1>Unauthorized User</h1>
+            <div className="App gradient-bg">
+                <div className="signInWrapper ">
+                    <div className="main-login-content">
+                    <img src={require("../images/firstTeeLogo.png")} alt="FirstTeeLogo" />
+                    <h1 className="name-header poppins-bold">Unauthorized User</h1>
+                    <h2 className="unauthorized poppins-light">Only coaches are allowed to access this application!</h2>
+                        <div>
+                        <button onClick={redirectToTarget}>Go back to Login Page</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
