9383ad4dc0dcf2dadbafd6c096ec87d1dc5243b8
Will Squibb <wsquibb@andrew.cmu.edu>
2024-04-02 10:50:31 -0400

    Add status icons

---
+++ client/src/icons/delivered.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Delivered() {
+  return (
+    <div>
+      <svg width="100px" height="100px" viewBox="0 -3 25 24" version="1.1" fill="#000000">
+        {/* Note that the g tags don't actually do anything unless they contain other elements */}
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
+        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/>
+        <g id="SVGRepo_iconCarrier">
+          <title>communication / 3 - communication, email, approve, check, envelope, mail, message icon</title>
+          <g stroke="#009de0" strokeWidth="1.575" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round">
+            {/* Adjust transform attributes as needed */}
+            <polyline points="21.001266 15 16 20.5 13.001266 17.4238742"/>
+            <polyline points="3.99663853 5 12 10 19.995112 5"/>
+            <path d="M9,18 C7.23084308,18 7.69638925,18 4.39663853,18 C3.62343988,18 2.99663853,17.4428432 2.99663853,16.7555556 L2.99663853,5.24444444 C2.99663853,4.55715676 3.62343988,4 4.39663853,4 L19.595112,4 C20.3683106,4 20.995112,4.55715676 20.995112,5.24444444 C20.9992147,7.80802349 21.001266,9.72654201 21.001266,11"/>
+          </g>
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+export default Delivered;
---
+++ client/src/icons/link_clicked.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function LinkClicked() {
+  return (
+    <div>
+      <svg width="100px" height="100px" viewBox="0 -4 20 24" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000">
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
+        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/>
+        <g id="SVGRepo_iconCarrier">
+          <title>communication / 11 - communication, account, verified, checked, person, profile, user icon</title>
+          <g id="Free-Icons" strokeWidth="1.5" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round">
+            <g transform="translate(-895.000000, -156.000000)" id="Group" stroke="#00ad0c" strokeWidth="1.5">
+              <g transform="translate(893.000000, 154.000000)" id="Shape">
+                <path d="M12,13 C9.23857625,13 7,10.7614237 7,8 C7,5.23857625 9.23857625,3 12,3 C14.7614237,3 17,5.23857625 17,8 C17,10.7614237 14.7614237,13 12,13 Z M11.0150512,21 C9.04777237,21 6.37608863,21 3,21 C3.79921286,17.89195 6.4614209,16.2328962 10.9866241,16.0228387"></path>
+                <polyline points="21.0037832 16.5969533 17 21 14.4906972 18.5114193"></polyline>
+              </g>
+            </g>
+          </g>
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+export default LinkClicked;
