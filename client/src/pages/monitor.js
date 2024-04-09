4d1c947825af5115e6606c02cad0d28897cdaacf
Will Squibb <wsquibb@andrew.cmu.edu>
2024-04-09 00:31:57 -0400

    Resolve name error

--- client/src/pages/monitor.js
+++ client/src/pages/monitor.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
-import ApiKeys from "../components/monitor/ApiKeys";
+import ApiKeys from "../components/monitor/APIKeys";
 
 const ControlCenterCard = () => {
   return (
