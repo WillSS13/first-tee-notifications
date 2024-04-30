62b0ee6974b9d2cede2bab59d068db965b38b8e1
Will Squibb <wsquibb@andrew.cmu.edu>
2024-04-30 07:32:01 -0400

    TODO: add Mailersend to tell notify of broken Knock API

--- server/test/testKnock.js
+++ server/test/testKnock.js
@@ -27,7 +27,7 @@ async function testConnection() {
     return;
   }
 
-  const knock = new Knock(process.env.KNOCK_API_KEY);
+  const knock = new Knock(api_key);
 
   console.log(knockColor(">> Testing Connection\n"));
   try {
@@ -35,7 +35,10 @@ async function testConnection() {
     console.log(arrow + "Successfully connected to Knock.");
     console.log(arrow + "Retrieved latest message ID: " + clc.bold(response.items[0].id));
   } catch (err) {
-    console.error("Failed to connect to Knock:", err);
+    let errorMessage = (err.message || "Unknown error");
+    let errorDetails = JSON.stringify(err, null, 2);
+    console.error(arrow + "Error: " + clc.redBright(errorMessage));
+    // TODO: SEND SYSTEM ALERT THROUGH MAILERSEND
   }
   
 }
