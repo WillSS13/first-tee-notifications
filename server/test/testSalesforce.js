7bfcefe946f232bf0babf2d0fecceb7a2796b146
Will Squibb <wsquibb@andrew.cmu.edu>
2024-04-30 04:04:16 -0400

    Test connections to all Salesforce tables, objects, & fields

--- server/test/testSalesforce.js
+++ server/test/testSalesforce.js
@@ -42,6 +42,65 @@ async function testConnection() {
   }
 }
 
+async function testContactTable() {
+  console.log(salesforceColor("\n>> Testing Access to Contact Table\n"));
+  try {
+    const result = await conn.sobject("Contact")
+      .select(`Id, Name, Email, Secondary_Email__c`)
+      .limit(1)
+      .execute();
+
+    console.log(arrow + "Sample Contact Name: ", clc.bold(result[0].Name));
+    console.log(arrow + "Sample Contact Email: ", clc.bold(result[0].Email));
+  } catch (err) {
+    console.error(arrow + "Failed to query Contact Table:", err);
+  }
+}
+
+async function testSessionRegistrationTable() {
+  console.log(salesforceColor("\n>> Testing Access to Session Registration Table\n"));
+  try {
+    const result = await conn.sobject("Session_Registration__c")
+      .select(`Id, Name, Status__c, Contact__c, Contact__r.Name, Contact__r.Primary_Contact_s_Email__c, Contact__r.Emergency_Contact_Number__c, Contact__r.Contact_Type__c, Contact__r.Participation_Status__c`)
+      .limit(1)
+      .execute();
+
+    console.log(arrow + "Sample Session Registration Name: ", clc.bold(result[0].Name));
+    console.log(arrow + "Sample Session Registration Contact: ", clc.bold(result[0].Contact__c));
+  } catch (err) {
+    console.error(arrow + "Failed to query Session Registration Table:", err);
+  }
+}
+
+async function testCoachAssignmentTable() {
+  console.log(salesforceColor("\n>> Testing Access to Coach Assignment Table\n"));
+  try {
+    const result = await conn.sobject("Coach_Assignment__c")
+      .select(`Id, Coach__c, Coach__r.Name, Name, Listing_Session__c,Session_End_Date__c, Session_Start_Date__c, Listing_Session__r.Id, Listing_Session__r.Name, Coach__r.Email, Coach__r.MobilePhone, Coach__r.Contact_Type__c`)
+      .limit(1)
+      .execute();
+
+    console.log(arrow + "Sample Coach Assignment Name: ", clc.bold(result[0].Name));
+    console.log(arrow + "Sample Coach Assignment Coach: ", clc.bold(result[0].Coach__c));
+  } catch (err) {
+    console.error(arrow + "Failed to query Coach Assignment Table:", err);
+  }
+}
+
+async function testListingSessionTable() {
+  console.log(salesforceColor("\n>> Testing Access to Listing Session Table\n"));
+  try {
+    const result = await conn.sobject("Listing_Session__c")
+      .select("Id, Total_Registrations__c")
+      .limit(1)
+      .execute();
+
+    console.log(arrow + "Sample Listing Session ID: ", clc.bold(result[0].Id));
+  } catch (err) {
+    console.error(arrow + "Failed to query Listing Session Table:", err);
+  }
+}
+
 // TODO: TEST THAT WE CAN RETRIEVE EACH TABLE & FIELDS FOR EACH FUNCTION
 // E.G. Coach_Assignments__c, Listing_Session__c, etc.
 
@@ -49,6 +108,10 @@ async function testConnection() {
 
 async function testSalesforce() {
   await testConnection();
+  await testContactTable();
+  await testSessionRegistrationTable();
+  await testCoachAssignmentTable();
+  await testListingSessionTable();
 }
 
 module.exports = { testSalesforce };
